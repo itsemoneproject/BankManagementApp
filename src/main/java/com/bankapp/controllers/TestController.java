e335ab4eb427b1c144592189439a28d89ddd372a
itsemoneproject <itsemoneproject@gmail.com>
2025-01-21 11:12:36 +0530

    added TestController.java

--- src/main/java/com/bankapp/controllers/TestController.java
+++ src/main/java/com/bankapp/controllers/TestController.java
@@ -1,12 +1,20 @@
 package com.bankapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 	@RequestMapping("/")
 	public String index() {
 		return "index.html";
+	}
+	
+	@RequestMapping("/greet/{name}")
+	@ResponseBody
+	public String greet(@PathVariable("name") String name) {
+		return "Hello " + name + "!";
 	}
 }
