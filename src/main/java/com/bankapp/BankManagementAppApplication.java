70643e90596dbb7a687934ec2a4414d293b044c3
itsemoneproject <itsemoneproject@gmail.com>
2025-01-21 10:56:04 +0530

    ready. set. go

---
+++ src/main/java/com/bankapp/BankManagementAppApplication.java
@@ -0,0 +1,14 @@
+package com.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
+public class BankManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementAppApplication.class, args);
+	}
+
+}
